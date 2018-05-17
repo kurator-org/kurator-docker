4ae676b06e1ca35f282b0f03d65c96976d61d45c
paopao74cn <zhangqian06@gmail.com>
2018-05-17 14:21:53 -0500

    enale file sharing functionality

--- kurator-tdwg17-yw/run.sh
+++ kurator-tdwg17-yw/run.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-docker run -it kurator/kurator-tdwg17-yw:0.1
+# docker run -it kurator/kurator-tdwg17-yw:0.1 
+docker run -v ~/Downloads/results/:/home/kurator/kurator-validation/packages/kurator_FileBranchingTaxonLookup/workflows/results -it kurator/kurator-tdwg17-yw:0.1 
+# docker run -v host_dir:docker_image_file_dir     % to share files/volumes/directories/folders between the docker container and the host machine, so that you are able to visualize the data files from your applications on your host machine.
